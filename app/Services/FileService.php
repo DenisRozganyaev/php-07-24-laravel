bec6be5c247602568045cb76e66b10aadf5cf44f
Denys Rozghaniaiev <denis.rozganyaev@gmail.com>
2024-10-31 20:29:51 +0200

    [lesson_27] tests (#12)

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -5,17 +5,20 @@ namespace App\Services;
 use App\Services\Contracts\FileServiceContract;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class FileService implements Contracts\FileServiceContract
 {
-
     public function upload(UploadedFile $file, string $additionalPath = ''): string
     {
         $additionalPath = !empty($additionalPath) ? $additionalPath . '/' : '';
 
-        $filePath = $additionalPath . time() . '_' . $file->getClientOriginalName();
+        $filePath =  Str::slug(microtime());
+        $filePath = $additionalPath . $filePath . '_' . $file->getClientOriginalName();
         Storage::put($filePath, File::get($file));
+        Storage::setVisibility($filePath, 'public');
 
         return $filePath;
     }
