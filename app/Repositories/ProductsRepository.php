0ad36c0118ac494d1d6ed2565334d062bf933fba
Denys Rozghaniaiev <denis.rozganyaev@gmail.com>
2024-10-31 20:29:34 +0200

    ProductsController, ImagesRepository (#7)

--- app/Repositories/ProductsRepository.php
+++ app/Repositories/ProductsRepository.php
@@ -5,11 +5,16 @@ namespace App\Repositories;
 use App\Http\Requests\Admin\Products\CreateRequest;
 use App\Http\Requests\Admin\Products\EditRequest;
 use App\Models\Product;
+use App\Repositories\Contracts\ImagesRepositoryContract;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ProductsRepository implements Contracts\ProductsRepositoryContract
 {
+    public function __construct(protected ImagesRepositoryContract $imagesRepository)
+    {
+    }
+
     public function store(CreateRequest $request): Product|false
     {
         try {
@@ -17,7 +22,7 @@ class ProductsRepository implements Contracts\ProductsRepositoryContract
 
             $data = $this->formRequestData($request);
             $product = Product::create($data['attributes']);
-            $product->categories()->sync($data['categories']);
+            $this->updateRelationData($product, $data);
 
             DB::commit();
 
@@ -31,17 +36,44 @@ class ProductsRepository implements Contracts\ProductsRepositoryContract
 
     public function update(Product $product, EditRequest $request): bool
     {
-        return false;
+        try {
+            DB::beginTransaction();
+
+            $data = $this->formRequestData($request);
+            $product->update($data['attributes']);
+            $this->updateRelationData($product, $data);
+
+            DB::commit();
+
+            return true;
+        } catch (\Throwable $exception) {
+            DB::rollBack();
+            logs()->error($exception);
+            return false;
+        }
+    }
+
+    protected function updateRelationData(Product $product, array $data): void
+    {
+        $product->categories()->sync($data['categories'] ?? []);
+
+        $this->imagesRepository->attach(
+            $product,
+            'images',
+            $data['images'] ?? [],
+            $product->imagesPath
+        );
     }
 
     protected function formRequestData(CreateRequest|EditRequest $request): array
     {
         return [
             'attributes' => collect($request->validated())
-                ->except(['categories'])
+                ->except(['categories', 'images'])
                 ->prepend(Str::slug($request->get('title')), 'slug')
                 ->toArray(),
-            'categories' => $request->get('categories', [])
+            'categories' => $request->get('categories', []),
+            'images' => $request->file('images', []),
         ];
     }
 }
