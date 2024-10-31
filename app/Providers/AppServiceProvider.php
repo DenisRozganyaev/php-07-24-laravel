ebe726e9e9604a104844d18b0e9a6bf4560b04ad
Denys Rozghaniaiev <denis.rozganyaev@gmail.com>
2024-10-31 20:29:30 +0200

    Admin Products (#6)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,29 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\ProductsRepositoryContract;
+use App\Repositories\ProductsRepository;
+use App\Services\Contracts\FileServiceContract;
+use App\Services\FileService;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $bindings = [
+        ProductsRepositoryContract::class => ProductsRepository::class,
+        FileServiceContract::class => FileService::class,
+    ];
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+//        app()->bind(ProductsRepositoryContract::class, ProductsRepository::class);
+//        app()->when(ProductsController::class)
+//            ->needs(ProductsRepositoryContract::class)
+//            ->give(ProductsRepository::class);
     }
 
     /**
